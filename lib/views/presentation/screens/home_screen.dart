f31051be3fefb7fcdf8f34bb5efc9b1e337529f8
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-08-19 19:25:56 +0300

    chore: Add logo to AppBar in HomeScreen

--- lib/views/presentation/screens/home_screen.dart
+++ lib/views/presentation/screens/home_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:lilac/utils/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+      appBar: AppBar(
+        
+        title: Image.asset(logo),
+       bottom:  const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.favorite), text: 'Favorites'),
+              Tab(icon: Icon(Icons.person), text: 'Profile'),
+            ],
+          ),
+      ),
+    );
   }
 }
