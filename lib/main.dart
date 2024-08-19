e6a872fe4f63e68e77ef8b4eaa860e751c78e675
Mahmoud Ahmed <UGS.141490@ci.suez.edu.eg>
2024-08-19 10:50:02 +0300

    chore: Refactor main.dart to remove commented code and disable debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,9 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      
-      home:HomeScreen() ,
+      home: HomeScreen(),
     );
   }
 }
